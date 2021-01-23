3f74debf961bb5980ad76c63b232763911bfcdb8
Jonah Dahlquist <hi@jonah.name>
2021-01-23 13:28:01 -0800

    Prototype of sending token to worker, researched and layed out payment processing steps

--- signup-worker/src/handler.ts
+++ signup-worker/src/handler.ts
@@ -1,3 +1,7 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  // https://stripe.com/docs/api
+  // TODO customers.create, populate `source` with the provided token ID
+  // TODO invoices.create for $5 registration fee
+  // TODO subscriptions.create with customer ID
+  return new Response(`request body: ${await request.text()}`, {headers: {'Access-Control-Allow-Origin': '*'}})
 }

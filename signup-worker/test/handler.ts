6d958bcfc28d6b92a69aa7a0749ffccfb985f116
Jonah Dahlquist <hi@jonah.name>
2021-01-23 12:42:51 -0800

    Replaced Rust crate with a TS cloudflare worker template

--- signup-worker/test/handler.ts
+++ signup-worker/test/handler.ts
@@ -1,4 +1,3 @@
-import {describe, it} from 'mocha';
 import { expect } from 'chai'
 import { handleRequest } from '../src/handler'
 
--- signup-worker/webpack.config.js
+++ signup-worker/webpack.config.js
@@ -4,9 +4,8 @@ const webpack = require('webpack')
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
-  entry: './src/index.ts',
   output: {
-    filename: `worker.js`,
+    filename: `worker.${mode}.js`,
     path: path.join(__dirname, 'dist'),
   },
   mode,
@@ -19,6 +18,9 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
       },
     ],
   },

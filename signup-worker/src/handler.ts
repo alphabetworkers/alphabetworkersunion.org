f32d2e29bab38b73cdb180e9b1eb676fb254c033
Jonah Dahlquist <hi@jonah.name>
2021-01-23 16:46:10 -0800

    Added basic customer creation with payment source token association

--- signup-worker/src/handler.ts
+++ signup-worker/src/handler.ts
@@ -1,7 +1,16 @@
+import {stripeRequest} from './stripe';
+
 export async function handleRequest(request: Request): Promise<Response> {
-  // https://stripe.com/docs/api
-  // TODO customers.create, populate `source` with the provided token ID
-  // TODO invoices.create for $5 registration fee
-  // TODO subscriptions.create with customer ID
-  return new Response(`request body: ${await request.text()}`, {headers: {'Access-Control-Allow-Origin': '*'}})
+  try {
+    const fields = await request.formData();
+    const response = await stripeRequest('customers', {
+      'source': fields.get('stripe-payment-token'),
+    });
+    // TODO invoices.create for $5 registration fee
+    // TODO subscriptions.create with customer ID
+    return new Response(`Created customer: ${await response.text()}`, {headers: {'Access-Control-Allow-Origin': '*'}})
+  } catch (e) {
+    console.log(e);
+    return new Response(`Failed: ${e}`, {headers: {'Access-Control-Allow-Origin': '*'}})
+  }
 }

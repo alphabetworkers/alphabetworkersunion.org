d1b7bdd965375d98702ac6e778d6b5d623227337
Jonah Dahlquist <hi@jonah.name>
2021-01-24 15:18:04 -0800

    Basic subscription creation and code cleanup

--- signup-worker/webpack.config.js
+++ signup-worker/webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
+  entry: './src/index.ts',
   output: {
     filename: `worker.js`,
     path: path.join(__dirname, 'dist'),
@@ -18,9 +19,6 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
-        },
       },
     ],
   },

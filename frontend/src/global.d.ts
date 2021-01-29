ab0b61068b9082b82d2eb55823e8c76f46067817
Jonah Dahlquist <hi@jonah.name>
2021-01-28 23:25:57 -0800

    Added style loader for lit-elements, improved styling of signup, bound employment type to employer field

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,7 @@
+// TODO remove if this is merged: https://github.com/drdreo/lit-scss-loader/pull/21
+
+declare module '*.scss' {
+  import { CSSResult } from 'lit-element';
+  const css: CSSResult;
+  export default css;
+}

8ffd5c0b1d5669f837847212e966c574d08ca2dc
Jonah Dahlquist <hi@jonah.name>
2021-03-18 22:03:54 -0700

    Moved to AWU cloudflare account.  Moved required fields into a single file referenced by both server and client code

---
+++ signup-worker/src/fields.ts
@@ -0,0 +1,37 @@
+// Dependency of both server and client sides.
+
+const REQUIRED_METADATA = [
+  'employment-type',
+  'signature',
+  'preferred-name',
+  'personal-email',
+  'personal-phone',
+  'mailing-address-1',
+  'mailing-city',
+  'mailing-region',
+  'mailing-postal-code',
+  'mailing-country',
+  'employer',
+  'site-code',
+  'org',
+  'team',
+  'job-title',
+  'have-reports',
+  'total-compensation',
+];
+
+export const REQUIRED_FIELDS = REQUIRED_METADATA.concat([
+  'preferred-name',
+  'personal-email',
+]);
+
+const OPTIONAL_METADATA = [
+  'pronouns',
+  'preferred-language',
+  'mailing-address-2',
+  'building-code',
+  'product-area',
+  'work-email',
+];
+
+export const METADATA = REQUIRED_METADATA.concat(OPTIONAL_METADATA);

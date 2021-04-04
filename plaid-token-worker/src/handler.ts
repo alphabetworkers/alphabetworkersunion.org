fa1be3e3bfbf47b466e2cb1341d8c2ad6c9e2ae2
Jonah Dahlquist <hi@jonah.name>
2021-04-04 14:50:50 -0700

    Added worker for getting a Link token from Plaid

---
+++ plaid-token-worker/src/handler.ts
@@ -0,0 +1,6 @@
+import {plaidClient} from './plaid';
+
+export async function handleRequest(request: Request): Promise<Response> {
+  return new Response(JSON.stringify(await plaidClient.linkTokenCreate()), {headers: {'Access-Control-Allow-Origin': '*'}});
+}
+

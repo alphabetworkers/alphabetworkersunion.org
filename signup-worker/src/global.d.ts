caa8c57533e5500721e26a1136e06689ae8c6ac1
Jonah Dahlquist <hi@jonah.name>
2021-04-04 20:34:11 -0700

    Added front-end Plaid Link UI and API params to support instant bank verification

--- signup-worker/src/global.d.ts
+++ signup-worker/src/global.d.ts
@@ -2,3 +2,7 @@
 declare const STRIPE_KEY: string;
 declare const DUES_PRODUCT_ID: string;
 declare const DUES_SIGNUP_PRICE_ID: string;
+
+declare const PLAID_CLIENT_ID: string;
+declare const PLAID_SECRET: string;
+declare const PLAID_ORIGIN: string;
---
+++ signup-worker/src/plaid.ts
@@ -0,0 +1,69 @@
+import {ItemPublicTokenExchangeRequest, ItemPublicTokenExchangeResponse, ProcessorStripeBankAccountTokenCreateRequest, ProcessorStripeBankAccountTokenCreateResponse} from 'plaid';
+
+// TODO consolidate with the code in ../../plaid-token-worker
+/**
+ * Because plaid-node does not work without the Node runtime, we must
+ * re-implement API calls directly to the Plaid RESTful API.  This project
+ * still depends on plaid-node for its type declarations.
+ */
+export class PlaidClient {
+  private readonly headers: {[key: string]: string};
+
+  /**
+   * Instantiate a client instance.  No overhead in destroying or creating, it
+   * only stores an API key.
+   */
+  constructor(clientId: string, secret: string) {
+    this.headers = Object.freeze({
+      'PLAID-CLIENT-ID': clientId,
+      'PLAID-SECRET': secret,
+      'Content-Type': 'application/json',
+    });
+  }
+
+  /**
+   * Sends a call to create a customer object.
+   *
+   * @link https://stripe.com/docs/api/customers/create?lang=node
+   */
+  itemPublicTokenExchange(public_token: string): Promise<ItemPublicTokenExchangeResponse> {
+    return fetch(`${PLAID_ORIGIN}/item/public_token/exchange`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        public_token,
+      } as ItemPublicTokenExchangeRequest),
+    }).then(throwError<ItemPublicTokenExchangeResponse>());
+  }
+
+  /**
+   * Sends a call to create a customer object.
+   *
+   * @link https://stripe.com/docs/api/customers/create?lang=node
+   */
+  processorStripeBankAccountTokenCreate(access_token: string, account_id: string): Promise<ProcessorStripeBankAccountTokenCreateResponse> {
+    return fetch(`${PLAID_ORIGIN}/processor/stripe/bank_account_token/create`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        access_token,
+        account_id,
+      } as ProcessorStripeBankAccountTokenCreateRequest),
+    }).then(throwError<ProcessorStripeBankAccountTokenCreateResponse>());
+  }
+}
+
+function throwError<T>(): (response: Response) => Promise<T> {
+  return async (response: Response) => {
+    if (response.ok) {
+      return response.json() as Promise<T>;
+    } else {
+      return Promise.reject((await response.json()));
+    }
+  };
+}
+
+/**
+ * Create a new re-usable instance.
+ */
+export const plaidClient = new PlaidClient(PLAID_CLIENT_ID, PLAID_SECRET);

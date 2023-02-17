bf3f943da29140a8eb1006a6690f74eb3bbb3781
moowiz <moowiz2020@gmail.com>
2023-02-16 23:39:38 -0800

    Format fix

--- frontend/src/element/signup.ts
+++ frontend/src/element/signup.ts
@@ -996,9 +996,9 @@ export class Signup extends LitElement {
             >Annual dues are 1% of your TC, plus processing fee if you opt to
             pay with a card. This is billed monthly, and is pooled and
             democratically controlled by you and your fellow members. See
-            <a href="/power/faq/#how-much-are-dues">the FAQ</a> for more details.
-            </span
-          >
+            <a href="/power/faq/#how-much-are-dues">the FAQ</a> for more
+            details.
+          </span>
           ${this.duesTemplate()}
         </label>
         ${this.plaidToken

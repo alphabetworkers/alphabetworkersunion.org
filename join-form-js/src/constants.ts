ed33df22c04f925f0159a4361b0bd6a5cf2c195a
Stephen McMurtry <stephenmcmurtry@Stephens-MacBook-Pro.local>
2023-06-08 20:38:41 -0500

    Move Join Form code to own directory

---
+++ join-form-js/src/constants.ts
@@ -0,0 +1,5 @@
+export const CARD_PROCESSING_FEE = 0.029;
+export const FRIENDLY_CARD_PROCESSING_FEE = '2.9%';
+
+export const INITIATION_FEE_CENTS = 500;
+export const FRIENDLY_INITIATION_FEE = '$5';

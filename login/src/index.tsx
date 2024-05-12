78b4752a42b79e4d4c77dbb536bf86754fbb8749
Jonah Bron <hi@jonah.id>
2024-05-12 06:54:02 +0000

    Looked up Stripe Customer ID by email for login token

--- login/src/index.tsx
+++ login/src/index.tsx
@@ -1,6 +1,7 @@
 import { loginPage } from './login-page';
 import { sendLoginEmail } from './sendgrid';
 import { sign } from '@tsndr/cloudflare-worker-jwt';
+import Stripe from 'stripe';
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -8,41 +9,53 @@ export default {
       const body = await request.formData();
       if (body.get('email')) {
         const email = body.get('email');
-        // TODO check for Stripe Customer object matching the email first.
-        try {
-          const loginLink = await makeLoginLink(request.url, email, env);
-          await sendLoginEmail(email, loginLink, env);
-        } catch (e) {
-          console.error(e);
-          const redirectUrl = new URL(request.url);
-          redirectUrl.searchParams.set('failure', 1);
-          return Response.redirect(redirectUrl.toString());
+        const customerId = getCustomerIdByEmail(email, env);
+        if (customerId) {
+          try {
+            const loginLink = await makeLoginLink(request.url, customerId, env);
+            await sendLoginEmail(email, loginLink, env);
+          } catch (e) {
+            console.error(e);
+            return Response.redirect(urlWithParam(request.url, 'failure'));
+          }
         }
-        const redirectUrl = new URL(request.url);
-        redirectUrl.searchParams.set('link_sent', 1);
-        return Response.redirect(redirectUrl.toString());
+        return Response.redirect(urlWithParam(request.url, 'link_sent'));
       }
     } else {
+      // TODO check for login_token parameter.
       return loginPage(new URL(request.url).searchParams);
     }
   },
 };
 
-function makeLoginToken(email: string, env: Env): Promise<string> {
+async function getCustomerIdByEmail(email: string, env: Env): string | undefined {
+  const stripe = new Stripe(env.STRIPE_API_KEY);
+  const customers = await stripe.customers.search({
+    query: `email:${JSON.stringify(email)}`,
+    limit: 1,
+  });
+  const customerId = customers.data[0]?.id;
+  return customerId;
+}
+
+function makeLoginToken(customerId: string, env: Env): Promise<string> {
   const secret = env.LOGIN_LINK_SECRET;
   if (!secret) throw new Error('LOGIN_LINK_SECRET must be a random 512-byte hex string.');
   return sign(
     {
-      email,
-      // TODO put Customer ID into the token instead of email.
+      stripeCustomerId: customerId,
       exp: Math.floor(Date.now() / 1000) + 60 * 30, // Expires in 30 min.
     },
     secret,
   );
 }
 
-async function makeLoginLink(incomingUrl: string, email: string, env: Env): Promise<string> {
-  const url = new URL(incomingUrl);
-  url.searchParams.set('login_token', await makeLoginToken(email, env));
-  return url.toString();
+function urlWithParam(url: string, param: string, value?: string = ''): string {
+  const newUrl = new URL(url);
+  newUrl.searchParams.set(param, value);
+  return newUrl.toString();
+}
+
+async function makeLoginLink(incomingUrl: string, customerId: string, env: Env): Promise<string> {
+  return urlWithParam(incomingUrl, 'login_token', await makeLoginToken(customerId, env));
 }

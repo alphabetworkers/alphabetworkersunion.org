bec024e8044016ed8b41738b43c8f44ed971e2bd
Jonah Bron <hi@jonah.id>
2024-05-14 06:15:05 +0000

    Added styling and fleshed out instructions for billing redirect

--- login/src/member-page.tsx
+++ login/src/member-page.tsx
@@ -6,53 +6,83 @@ export async function memberPage(customerId: string, env: Env): Promise<Response
   return makeHtmlResponse(
     renderDocument(
       sourceId ? (
-        <form method="post" action="" style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}>
-          <p style={{
-            background: '#fff7bb',
-            border: 'solid 2px #ffdc5d',
-            padding: 16,
-            borderRadius: 8,
-            maxWidth: 'min(700px, 90vw)',
-          }}>
+        <form
+          method="post"
+          action=""
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <p
+            style={{
+              background: '#fff7bb',
+              border: 'solid 2px #ffdc5d',
+              padding: 16,
+              borderRadius: 8,
+              maxWidth: 'min(700px, 90vw)',
+            }}
+          >
             ℹ️
             <strong>Legacy billing must be removed first</strong>
             <br />
             <br />
-            Your bank details are linked via a method that Stripe has
-            deprecated.
+            Your bank details are linked via a method that Stripe has deprecated.
             <br />
             <br />
-            Before you can manage your billing details, the current payment
-            source must be removed.  Please be sure to add a new billing method
-            shortly after.
+            Before you can manage your billing details, the current payment source must be removed. Please be sure to add a new billing
+            method shortly after.
           </p>
-          <button type="submit" name="delete_source" value="1" style={{
+          <button
+            type="submit"
+            name="delete_source"
+            value="1"
+            style={{
               fontSize: '1.1em',
               background: '#b0171d',
               padding: '20px 30px',
               border: 0,
               borderRadius: 50,
               color: 'white',
-          }}>
+            }}
+          >
             Delete payment source
           </button>
-          <p style={{
-            padding: 16,
-            borderRadius: 8,
-            maxWidth: 'min(700px, 90vw)',
-          }}>
+          <p
+            style={{
+              padding: 16,
+              borderRadius: 8,
+              maxWidth: 'min(700px, 90vw)',
+            }}
+          >
             You'll be taken straight to the billing portal.
           </p>
         </form>
       ) : (
-        <>
-          Click here to navigate to Stripe's customer portal, where you can manage your billing information.
-          <a href="/stripe-portal">Go to Stripe Portal</a>
-        </>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <p>Your billing details can be managed in the Stripe billing portal.</p>
+          <a
+            href="/stripe-portal"
+            style={{
+              fontSize: '1.1em',
+              background: '#b0171d',
+              padding: '20px 30px',
+              border: 0,
+              borderRadius: 50,
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            Open Stripe billing portal
+          </a>
+        </div>
       ),
     ),
   );
